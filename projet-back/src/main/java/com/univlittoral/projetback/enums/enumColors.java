a3b67363461410bd8935a8023a5fcc66f76140d3
gduquesne02 <85985528+gduquesne02@users.noreply.github.com>
2022-12-26 21:11:12 +0100

    Delete projet-back directory

--- projet-back/src/main/java/com/univlittoral/projetback/enums/enumColors.java
+++
@@ -1,5 +0,0 @@
-package com.univlittoral.projetback.enums;
-
-public enum enumColors {
-	RED, BLUE, YELLOW, GREY, GREEN
-}
--- projet-back/src/main/java/com/univlittoral/projetback/enums/enumGenre.java
+++
@@ -1,5 +0,0 @@
-package com.univlittoral.projetback.enums;
-
-public enum enumGenre {
-	BD, NOUVELLE, ROMAN, MANGA, POESIE
-}
